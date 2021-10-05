7d8ad21d61f7679652e476731a2da0575da95767
ahmed <a.awad@eightytech.com>
2021-10-05 20:41:22 +0200

    added initial migration for tables

---
+++ database/migrations/2021_10_05_183925_create_tables_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTablesTable extends Migration
+{
+    private string $table;
+
+    public function __construct()
+    {
+        $this->table = 'tables';
+    }
+
+    public function up()
+    {
+        Schema::create($this->table, function (Blueprint $table) {
+            $table->id();
+            $table->tinyInteger('capacity');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists($this->table);
+    }
+}
