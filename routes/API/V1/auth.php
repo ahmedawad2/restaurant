98fc1b8d7eb2b2517f84a6e30dda706907a60783
ahmed <a.awad@eightytech.com>
2021-10-05 21:32:23 +0200

    registered AuthenticateCustomers and use it on logout route

--- routes/API/V1/auth.php
+++ routes/API/V1/auth.php
@@ -6,5 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix' => 'v1/auth'], function () {
     Route::post('register', [AuthController::class, 'register']);
     Route::post('verifyOTP', [AuthController::class, 'verifyOTP']);
-    Route::post('login', [AuthController::class, 'login']);
+    Route::post('login', [AuthController::class, 'login'])->name('login');
+
+    Route::group(['middleware' => ['AuthenticateGuardCustomers']], function () {
+        Route::post('logout', [AuthController::class, 'logout']);
+    });
 });
