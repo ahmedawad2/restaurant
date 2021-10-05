1ef5a2b5ddd107a0796523d63dbfffbe60efb41e
ahmed <a.awad@eightytech.com>
2021-10-05 19:39:33 +0200

    added a provider for interfaces initial binding

---
+++ app/Providers/InterfacesBindingServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace App\Providers;
+
+
+use App\Infra\Classes\Common\PhoneValidator;
+use App\Infra\Interfaces\Repositories\Common\CustomerRepositoryInterface;
+use App\Infra\Interfaces\Resources\ResourceInterface;
+use App\Infra\Interfaces\Validators\FilesValidatorInterface;
+use App\Infra\Interfaces\Validators\PhoneValidatorInterface;
+use App\Infra\Interfaces\Validators\ValidatorInterface;
+use App\Infra\Repositories\Common\CustomerRepository;
+use App\Infra\Resources\RequestResource;
+use App\Infra\Validators\Validator;
+use Illuminate\Support\ServiceProvider;
+
+class InterfacesBindingServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $this->app->bind(ResourceInterface::class, RequestResource::class);
+        $this->app->bind(FilesValidatorInterface::class, Validator::class);
+        $this->app->bind(ValidatorInterface::class, Validator::class);
+        $this->app->bind(PhoneValidatorInterface::class, PhoneValidator::class);
+
+        //repositories
+        $this->app->bind(CustomerRepositoryInterface::class, CustomerRepository::class);
+
+    }
+
+    public function boot()
+    {
+
+    }
+}
