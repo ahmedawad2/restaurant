1797b2088429859a323dde05dfb55614c7055ed2
ahmed <a.awad@eightytech.com>
2021-10-06 23:18:55 +0200

    fix naming issues

--- database/migrations/2021_10_06_205511_create_orders_table.php
+++ database/migrations/2021_10_06_205511_create_orders_table.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CerateOrdersTable extends Migration
+class CreateOrdersTable extends Migration
 {
     private string $table;
 
