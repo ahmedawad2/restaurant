689f0b1adf7fa9954b5fe6fcde7e2fe557df3ca8
ahmed <a.awad@eightytech.com>
2021-10-06 22:51:54 +0200

    depend on code as a PK for promo codes table, add necessary properties for eloquent to work properly

--- app/Models/PromoCode.php
+++ app/Models/PromoCode.php
@@ -7,6 +7,9 @@ class PromoCode extends Common
 {
     protected $guarded = ['id'];
     protected $dates = ['from', 'to'];
+    protected $primaryKey = 'id';
+    protected $keyType = 'string';
     public $timestamps = false;
+    public $incrementing = false;
 }
 
