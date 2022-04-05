1aa9fec49077b6ef0c9b5f765c8685857ff112a3
ahmed <a.awad@eightytech.com>
2022-04-05 17:53:32 +0200

    indentation

--- app/Infra/Interfaces/Repositories/TablesRepositoryInterface.php
+++ app/Infra/Interfaces/Repositories/TablesRepositoryInterface.php
@@ -10,6 +10,8 @@ use App\Infra\Interfaces\Crud\UpdateInterface;
 interface TablesRepositoryInterface extends CrudInterface, RepositoryInterface, UpdateInterface
 {
     public function forActiveReservation(string $from, string $to, int $capacity): ?array;
+
     public function forWaitingReservation(string $from, string $to, int $capacity): ?array;
+
     public function forOverWaitingReservation(string $from, string $to, int $capacity): int;
 }
